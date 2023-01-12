bcd69c1ec7557ee3c661a1f66d15ee60ffa12170
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2023-01-11 23:49:36 -0500

    add MStack hasBalancedEnclosures method

--- src/com/javapractice/MStack.java
+++ src/com/javapractice/MStack.java
@@ -17,4 +17,56 @@ public class MStack {
         }
         return reversed.toString();
     }
+
+    static public boolean hasBalancedEnclosures(String expr) {
+        var enclosures = new Stack<Character>();
+        for (Character ch: expr.toCharArray()) {
+            if (isEnclosureOpening(ch)) {
+                enclosures.push(ch);
+            } else if (isEnclosureClosing(ch)) {
+                var lastEnc = enclosures.pop();
+                if (!isMatchingEnclosure(lastEnc, ch)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean isEnclosureOpening(Character ch) {
+        var isEnclosure = false;
+        var enclosures = "([<{";
+
+        for (Character enclosure: enclosures.toCharArray()) {
+            if (enclosure.equals(ch)) {
+                isEnclosure = true;
+            }
+        }
+
+        return isEnclosure;
+    }
+
+    private static boolean isEnclosureClosing(Character ch) {
+        var isEnclosure = false;
+        var enclosures = ")]>}";
+
+        for (Character enclosure: enclosures.toCharArray()) {
+            if (enclosure.equals(ch)) {
+                isEnclosure = true;
+            }
+        }
+
+        return isEnclosure;
+    }
+
+    private static boolean isMatchingEnclosure(Character opening,
+                                         Character closing) {
+        var o = opening.toString();
+        var c = closing.toString();
+        if (o.equals("(") && c.equals(")")) return true;
+        if (o.equals("[") && c.equals("]")) return true;
+        if (o.equals("<") && c.equals(">")) return true;
+        if (o.equals("{") && c.equals("}")) return true;
+        return false;
+    }
 }
--- src/com/javapractice/Main.java
+++ src/com/javapractice/Main.java
@@ -3,22 +3,26 @@ package com.javapractice;
 public class Main {
 
     public static void main(String[] args) {
-	var LL = new LinkedList("Hello");
-    LL.addTail("World");
-    LL.addTail("NY");
-    LL.addTail("TOKYO");
+//	var LL = new LinkedList("Hello");
+//    LL.addTail("World");
+//    LL.addTail("NY");
+//    LL.addTail("TOKYO");
 //    LL.removeHead();
 //    LL.removeHead();
 //    LL.reverse();
-    System.out.println(LL.getNthFromEnd(2));
-    System.out.println(LL.getNthFromEnd(1));
-    System.out.println(LL.getNthFromEnd(0));
-    System.out.println(LL.findMiddle());
-
-    LL.addTail("PARIS");
-    LL.addTail("BEIJING");
-    System.out.println(LL.findMiddle());
-    System.out.println(MStack.reverse("elephant"));
+//    System.out.println(LL.getNthFromEnd(2));
+//    System.out.println(LL.getNthFromEnd(1));
+//    System.out.println(LL.getNthFromEnd(0));
+//    System.out.println(LL.findMiddle());
+//
+//    LL.addTail("PARIS");
+//    LL.addTail("BEIJING");
+//    System.out.println(LL.findMiddle());
+//    System.out.println(MStack.reverse("elephant"));
+    var encTestA = "((hello [John] and <Wendy>, I see you are {talking}))";
+    var invalidEnc = "((hello [John] and <Wendy>, I see you are talking}))";
+    System.out.println(MStack.hasBalancedEnclosures(encTestA));
+    System.out.println(MStack.hasBalancedEnclosures(invalidEnc));
 //    System.out.println(LL.contains("NY"));
 //    System.out.println(LL.contains("TOKYO"));
 //    System.out.println(LL.contains("???"));
